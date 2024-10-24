736e6cf05cc05648e7c9bb972d1f5e88608f92b6
Chenfei_Wang <miffyeewang@gmail.com>
2024-10-24 14:04:16 +0200

    modify training.py

--- src/modelling/training.py
+++ src/modelling/training.py
@@ -1,3 +1,14 @@
+import os
+import mlflow
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from typing import List
+
 def train_model(
     X_train: np.ndarray, 
     y_train: np.ndarray
