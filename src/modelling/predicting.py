138dd67e81bddbc0bc1b81b4ce57b78cc1bdfafb
kuokuo <rekxh@icloud.com>
2024-10-24 13:38:49 +0200

    Update predicting.py

---
+++ src/modelling/predicting.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.pipeline import Pipeline
+
+
+def predict_age(
+    input_data: np.ndarray, 
+    pipeline: Pipeline
+    ):
+    
+    return pipeline.predict(input_data)
