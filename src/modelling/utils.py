b7d731322499864f4f9425498dd4208d107effe2
Chenfei_Wang <miffyeewang@gmail.com>
2024-10-24 15:31:46 +0200

    reformat

--- src/modelling/utils.py
+++ src/modelling/utils.py
@@ -1,7 +1,9 @@
 import pickle
 from pathlib import Path
 from typing import Any
+
 import pandas as pd
+
 
 def pickle_object(obj: Any, path: str) -> None:
     try:
@@ -10,6 +12,7 @@ def pickle_object(obj: Any, path: str) -> None:
         print(f"Object successfully pickled and saved to {path}")
     except Exception as e:
         print(f"Error pickling the object: {e}")
+
 
 def load_data(data_path: Path) -> pd.DataFrame:
     return pd.read_csv(data_path)
