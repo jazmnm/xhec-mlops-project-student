3e744cd2379332816fc84a84e81172c5c1af6023
Mingyu WANG <mingyu.wang@hec.edu>
2024-10-24 23:26:56 +0200

    rmse change import

--- src/modelling/predicting.py
+++ src/modelling/predicting.py
@@ -1,5 +1,4 @@
 import numpy as np
-from sklearn.metrics import root_mean_squared_error
 from sklearn.pipeline import Pipeline
 
 
@@ -7,5 +6,7 @@ def predict_age(input_data: np.ndarray, pipeline: Pipeline):
     return pipeline.predict(input_data)
 
 
-def evaluate_model(y_pred: np.ndarray, y_test: np.ndarray):
-    return root_mean_squared_error(y_test, y_pred)
+def evaluate_model(y_test, y_pred):
+    mse = np.mean((y_test - y_pred) ** 2)
+    rmse = np.sqrt(mse)
+    return rmse
