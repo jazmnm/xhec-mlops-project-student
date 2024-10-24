c19105fdb4d95a632086f04baa5656a28bf4b0b5
Rong SHEN <rong.shen@hec.edu>
2024-10-24 14:05:18 +0200

    reformat

--- src/modelling/training.py
+++ src/modelling/training.py
@@ -1,12 +1,8 @@
-def train_model(
-    X_train: np.ndarray, 
-    y_train: np.ndarray
-    ):
+def train_model(X_train: np.ndarray, y_train: np.ndarray):
 
-    pipeline = Pipeline([
-        ('scaler', StandardScaler()),
-        ('regressor', LinearRegression())
-    ])
+    pipeline = Pipeline(
+        [("scaler", StandardScaler()), ("regressor", LinearRegression())]
+    )
 
     pipeline.fit(X_train, y_train)
     return pipeline
